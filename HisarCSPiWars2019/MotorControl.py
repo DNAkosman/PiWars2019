239d56590f1108759723e4159d7df353b9aef9b2
yoelkastro <32455293+yoelkastro@users.noreply.github.com>
2019-02-04 16:21:26 +0300

    Fix controllerDataToMotorSpeed

--- HisarCSPiWars2019/MotorControl.py
+++ HisarCSPiWars2019/MotorControl.py
@@ -25,13 +25,19 @@ class MotorControl:
         motors.setSpeeds(rightSpeed, leftSpeed)
 
     def controllerDataToMotorSpeed(self, x, y, t):
-        if (t):
-            if (math.copysign(1, x) != math.copysign(1, y)):
-                return (int)((-y + x) * 240)
-            else:
-                return (int)((-y + x) * 480)
-        else:
-            if (math.copysign(1, x) == math.copysign(1, y)):
-                return (int)((-y - x) * 240)
-            else:
-                return (int)((-y - x) * 480)
+        
+        r = math.hypot(x, y)
+        t = math.atan2(y, x)
+
+        t += math.pi / 4
+
+        left = r * math.cos(t)
+        right = r * math.sin(t)
+
+        left = left * math.sqrt(2)
+        right = right * math.sqrt(2)
+ 
+        left = max(-1, min(left, 1))
+        right = max(-1, min(right, 1))
+
+        return int(left * 480), -int(right * 480)
